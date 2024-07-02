2ef7478a5f96e80b230c4df35cb8accb88019945
zouls99 <zulfikarfaras@gmail.com>
2024-07-02 17:19:58 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@ function show_form() {
     let form_el = document.querySelector("#form1");
 
     form_el.style.display = "block";
+
+    document.getElementById("fnama").value = document.getElementById("nama").innerText;
+    document.getElementById("frole").value = document.getElementById("role").innerText;
+    document.getElementById("favail").value = document.getElementById("avail").innerText;
+    document.getElementById("fage").value = document.getElementById("age").innerText;
+    document.getElementById("floc").value = document.getElementById("loc").innerText;
+    document.getElementById("fexp").value =  document.getElementById("exp").innerText;
+    document.getElementById("femail").value = document.getElementById("email").innerText;
 }
 
 function hide_form() {
